22b4832dc5385d43edd33d4810926d31a945a108
naveen <naveenreddy.k07@gmail.com>
2022-01-06 15:32:25 +0530

    all done

---
+++ DataStrcture1/LinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStrcture1
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        internal void add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next !=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linkedlist", node.data);
+        }
+        internal void display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("linkedlist is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+        internal Node InsertAtParticularPosition(int position,int data)
+        {
+            if(position<1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            if(position==1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while(position !=0)
+                {
+                    if(position==1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if(position!=1)
+                {
+                    Console.WriteLine("Position is out of Range");
+                }
+            }
+            return head;
+        }
+    }
+}
---
+++ DataStrcture1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStrcture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.add(56);
+            list.add(30);
+            list.add(70);
+            //list.display();
+            list.InsertAtParticularPosition(2, 30);
+        }
+    }
+}
